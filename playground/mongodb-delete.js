c0abe8851fc52b6b2c11a465cd058f3524227707
Ron Mapue <ronmapue@MacBook-Pro.hitronhub.home>
2018-01-05 21:05:19 -0800

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,28 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err){
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // deleteMany
+  // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
+  // deleteOne
+  // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
+  // findOneAndDelete
+  // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+  //   console.log(result);
+  // });
+  db.collection('Users').deleteMany({name: 'Jack Daniels'});
+  db.collection('Users').deleteOne({_id: new ObjectID('5a4f15764365bb648a24a048')}).then((result) => {
+    console.log(JSON.stringify(results, undefined, 2));
+  });
+
+  // db.close();
+});
